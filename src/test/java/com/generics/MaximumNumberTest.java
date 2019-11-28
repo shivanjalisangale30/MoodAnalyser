10107ec338be25d2407e54d62021ba258f32538e
shivanjalisangale30 <shivanjalisangale5456@gmail.com>
2019-11-28 15:49:30 +0530

    Using generics find maximum number

--- src/test/java/com/generics/MaximumNumberTest.java
+++ src/test/java/com/generics/MaximumNumberTest.java
@@ -3,7 +3,7 @@ package com.generics;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class MaximumNumberTest <E> {
+public class MaximumNumberTest<E> {
     @Test
     public void givenIntegerThreeNumber_whenProper_shouldGiveMaximumNumber() {
         MaximumNumber maximumNumber = new MaximumNumber();
@@ -16,22 +16,22 @@ public class MaximumNumberTest <E> {
 
     @Test
     public void givenThreeDoubleNumber_whenProper_shouldGiveMaximumNumber() {
-        MaximumNumber maximumNumber = new MaximumNumber();
+        MaximumNumber<Double> maximumNumber = new MaximumNumber<>();
         Double a = 1.5;
         Double b = 1.3;
         Double c = 1.4;
-        Double result = (Double) maximumNumber.maximumNumberFind(a, b, c);
+        Double result = maximumNumber.maximumNumberFind(a, b, c);
         Assert.assertEquals(a, result);
     }
 
     @Test
-    public  void givenThreeNumberRandom_whenProper_shouldGiveMaximumNumber() {
+    public void givenThreeCharacter_whenProper_shouldGiveMaximumCharacter() {
         MaximumNumber maximumNumber = new MaximumNumber();
         Character a = 'a';
         Character b = 'b';
         Character c = 'c';
         Character result = (Character) maximumNumber.maximumNumberFind(a, b, c);
         Assert.assertEquals(c, result);
-
     }
+
 }
